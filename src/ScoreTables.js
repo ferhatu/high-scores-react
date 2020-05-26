f48f1dba94a9959b0ce119885be2946c011d0083
ferhatu <ferhat_2001@yahoo.com>
2020-05-26 13:20:59 +0100

    update

--- src/ScoreTables.js
+++ src/ScoreTables.js
@@ -2,27 +2,29 @@ import React from "react";
 const ScoreTables = (props) => (
   <div className="App">
     <h1 className="heading">High Scores per Country</h1>
-    {props.generalScores.map((data) => {
-      return (
-        <div className="scores-container">
-          <div className="country-name">
-            <span>{data.name}</span>{" "}
+    <div className="all-scores">
+      {props.generalScores.map((data) => {
+        return (
+          <div className="scores-container">
+            <div className="country-name">
+              <span>HIGH SCORES: {data.name}</span>{" "}
+            </div>
+            {data.scores.map((data) => {
+              return (
+                <div className="scores-ns">
+                  <div className="scores-n">
+                    <span>{data.n.toUpperCase()}</span>
+                  </div>
+                  <div className="scores-s">
+                    <span> {data.s}</span>
+                  </div>
+                </div>
+              );
+            })}
           </div>
-          {data.scores.map((data) => {
-            return (
-              <div className="scores-ns">
-                <div className="scores-n">
-                  <span>{data.n.toUpperCase()}</span>
-                </div>
-                <div className="scores-s">
-                  <span> {data.s}</span>
-                </div>
-              </div>
-            );
-          })}
-        </div>
-      );
-    })}
+        );
+      })}
+    </div>
   </div>
 );
 export default ScoreTables;
