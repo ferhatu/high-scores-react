d1602578da7567a35eb05f6050a0d5bb0bf26ba7
ferhatu <ferhat_2001@yahoo.com>
2020-05-28 11:39:41 +0100

    level-1-2-3

--- src/ScoreTables.js
+++ src/ScoreTables.js
@@ -3,27 +3,31 @@ const ScoreTables = (props) => (
   <div className="App">
     <h1 className="heading">High Scores per Country</h1>
     <div className="all-scores">
-      {props.generalScores.map((data) => {
-        return (
-          <div className="scores-container">
-            <div className="country-name">
-              <span>HIGH SCORES: {data.name}</span>{" "}
+      {props.generalScores
+        .sort((a, b) => a.name.localeCompare(b.name))
+        .map((data) => {
+          return (
+            <div className="scores-container">
+              <div className="country-name">
+                <span>HIGH SCORES: {data.name}</span>{" "}
+              </div>
+              {data.scores
+                .sort((x, y) => y.s - x.s)
+                .map((data) => {
+                  return (
+                    <div className="scores-ns">
+                      <div className="scores-n">
+                        <span>{data.n.toUpperCase()}</span>
+                      </div>
+                      <div className="scores-s">
+                        <span> {data.s}</span>
+                      </div>
+                    </div>
+                  );
+                })}
             </div>
-            {data.scores.map((data) => {
-              return (
-                <div className="scores-ns">
-                  <div className="scores-n">
-                    <span>{data.n.toUpperCase()}</span>
-                  </div>
-                  <div className="scores-s">
-                    <span> {data.s}</span>
-                  </div>
-                </div>
-              );
-            })}
-          </div>
-        );
-      })}
+          );
+        })}
     </div>
   </div>
 );
